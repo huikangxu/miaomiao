79de9f86d05232e80efac2e74bf565cdf93fba2c
huikangxu <xuhkang@126.com>
2019-06-13 22:54:03 +0800

    components first

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import movieRouter from './movie'
+import cinemaRouter from './cinema'
+import mineRouter from './mine'
 
 Vue.use(Router)
 
 export default new Router({
+  mode : 'history',
+  base :　process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+  	movieRouter,
+  	cinemaRouter,
+  	mineRouter,
+  	{   //  这是重定向？当路径都不匹配时，默认进入movie
+  		path : '/*',
+  		redirect : '/movie'
+  	}
   ]
 })
